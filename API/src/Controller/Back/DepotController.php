c9a8887c97a4ae3dd0381ff1c5c408d90fe82efa
Florent-Genestier <florentgenestier@gmail.com>
2022-08-18 17:32:11 +0200

    User/Product/Depot Controller + Form

--- API/src/Controller/Back/DepotController.php
+++ API/src/Controller/Back/DepotController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Depot;
+use App\Repository\DepotRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/back/depot")
+ */
 class DepotController extends AbstractController
 {
     /**
---
+++ API/src/Controller/Back/UserController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/back/user")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="app_back_user_index, methods={"GET"})
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('back/user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_back_user_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // actuellement le mot de passe n'est pas haché dans le user
+            // on récupère le service UserPasswordHasherInterface 
+            // pour hasher le mot de passe à la mano
+            $passwordClear = $user->getPassword();
+            $hashedPassword = $passwordHasher->hashPassword($user, $passwordClear);
+            $user->setPassword($hashedPassword);
+
+            // cette méthode fait le persist et le flush !
+            $userRepository->add($user, true);
+
+            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_back_user_show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('back/user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_back_user_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Security $security, User $user, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository): Response
+    {
+        // $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('USER_UPDATE', $user);
+
+        // USER_UPDATE
+        // si l'utilisateur que l'on édite a le role manager ou admin
+        $form = $this->createForm(UserType::class, $user);
+        // $form->remove('password');
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // est ce qu'un mot de passe a été saisi
+
+            $passwordClear = $form->get('password')->getData();
+            if (! empty($passwordClear))
+            {
+                // si oui alors le hashé et le remplacer dans l'objet user
+                $hashedPassword = $passwordHasher->hashPassword($user, $passwordClear);
+                $user->setPassword($hashedPassword);
+            }
+
+            $userRepository->add($user, true);
+
+            return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+      /**
+     * @Route("/{id}", name="app_back_user_delete", methods={"POST"})
+     */
+    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('app_back_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+}
