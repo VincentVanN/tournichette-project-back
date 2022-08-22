68ac04d1d4ff806a4c7f67fedda528ad6aa29aec
Florent-Genestier <florentgenestier@gmail.com>
2022-08-22 13:23:33 +0200

    Controller routing category

--- API/src/Controller/CategoryController.php
+++ API/src/Controller/CategoryController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,8 +18,79 @@ class CategoryController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        return $this->render('back/category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
+
+    /**
+     * @Route("/new", name="app_back_category_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on slugify le titre fournit par le user avant de l'enregistrer en BDD
+            // plus besoin car on a fait un écouteur d'événements
+            // $category->setSlug($mySlugger->slugify($category->getTitle()));
+
+            $categoryRepository->add($category, true);
+
+            return $this->redirectToRoute('app_back_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/category/new.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_back_category_show", methods={"GET"})
+     */
+    public function show(Category $category): Response
+    {
+        return $this->render('back/category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_back_category_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Category $category, CategoryRepository $categoryRepository): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on slugify le titre fournit par le user avant de l'enregistrer en BDD
+            // $category->setSlug($mySlugger->slugify($category->getTitle()));
+            $categoryRepository->add($category, true);
+
+            return $this->redirectToRoute('app_back_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_back_category_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Category $category, CategoryRepository $categoryRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $categoryRepository->remove($category, true);
+        }
+
+        return $this->redirectToRoute('app_back_category_index', [], Response::HTTP_SEE_OTHER);
+    }
+
 }
+
