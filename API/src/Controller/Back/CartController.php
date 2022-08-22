b92ac37ca81928f4fae0c6e0d414a6fcacff9279
Florent-Genestier <florentgenestier@gmail.com>
2022-08-22 14:58:39 +0200

    maj product type

--- API/src/Controller/Back/CartController.php
+++ API/src/Controller/Back/CartController.php
@@ -15,13 +15,112 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    /**
-     * @Route("/cart", name="app_cart")
+    
+
+/**
+     * @Route("/", name="app_back_product_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+        return $this->render('back/product/index.html.twig', [
+            'products' => $productRepository->findAll(),
         ]);
     }
-}
+
+    /**
+     * @Route("/new", name="app_back_product_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ProductRepository $productRepository): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on slugify le titre fournit par le user avant de l'enregistrer en BDD
+            // plus besoin car on a fait un écouteur d'événements
+            // $product->setSlug($mySlugger->slugify($product->getTitle()));
+
+            $productRepository->add($product, true);
+
+            return $this->redirectToRoute('app_back_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/product/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_back_product_show", methods={"GET"})
+     */
+    public function show(product $product): Response
+    {
+        return $this->render('back/product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_back_product_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, product $product, ProductRepository $productRepository): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on slugify le titre fournit par le user avant de l'enregistrer en BDD
+            // $product->setSlug($mySlugger->slugify($product->getTitle()));
+            $productRepository->add($product, true);
+
+            return $this->redirectToRoute('app_back_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_back_product_delete", methods={"POST"})
+     */
+    public function delete(Request $request, product $product, ProductRepository $productRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $productRepository->remove($product, true);
+        }
+
+        return $this->redirectToRoute('app_back_product_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    public function record(?int $id = null)
+    {
+        $product = $id === null ? new Category() : Category::find($id);
+        $product ->setName($name);
+        $product ->setSlug($slug);
+        $product ->setStock($stock);
+        $product ->setUnity($unity);
+        $product ->setPrice($price);
+        $product ->setQuantity($quantity);
+        $product ->setCategory($category);
+
+        //if there is no error
+        if (empty($errors)) {
+            // we saved in BDD
+            if ($category->save()) {
+                if ($id === null) {
+                    // Si la sauvegarde a fonctionné, on redirige vers la liste des catégories.
+                    return $this->redirectToRoute('app_back_product_index', [], Response::HTTP_SEE_OTHER);
+                } else {
+                    // Si la sauvegarde a fonctionné, on redirige vers le formulaire d'édition en mode GET
+                    return $this->redirectToRoute('app_back_product_edit', [], Response::HTTP_SEE_OTHER);
+                }
+            } else {
+                $errors[] = "La sauvegarde a échoué";
+            }
+        }
+    }
+    
--- API/src/Form/ProductType.php
+++ API/src/Form/ProductType.php
@@ -13,12 +13,13 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('slug')
-            ->add('stock')
-            ->add('unity')
-            ->add('price')
-            ->add('category');
+        ->add('name',TextType::class,['label' => 'name '])
+        ->add('slug', TextType::class, ['label' => 'slug : '])
+            ->add('stock', TextType::class, ['label' => 'slug : '])
+            ->add('unity', TextType::class, ['label' => 'unity : '])
+            ->add('price',TextType::class, ['label' => 'price : '])
+            ->add('category', TextType::class, ['label' => 'category : ']);
+           
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -27,4 +28,11 @@ class ProductType extends AbstractType
             'data_class' => Product::class,
         ]);
     }
+
+    ->add('name',TextType::class,['label' => 'name '])
+            ->add('pa',TextType::class, ['label' => 'Prix Achat : '])
+            ->add('pv',TextType::class, ['label' => 'Prix Vente : '])
+            ->add('tva',TextType::class,['label' => 'Tva : '])
+            ->add('stock',TextType::class,['label' => 'Stock : '])
+            ->add('image' , FileType::class, ['label' => 'Image : '] );
 }
