1e30b18d88a6df2126e7af23ea672857bbdc280f
Florent-Genestier <florentgenestier@gmail.com>
2022-08-23 15:10:37 +0200

    nav link  product + order

--- API/src/Controller/Back/ProductController.php
+++ API/src/Controller/Back/ProductController.php
@@ -4,13 +4,15 @@ namespace App\Controller\Back;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Entity\OrderProduct;
+use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 /**
