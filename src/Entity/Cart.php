f8e7b1e09d7aa2f2cbdb29b3c088c532b23a790c
Steve-Amat <steve.sfa.amat@free.fr>
2022-08-31 11:57:57 +0200

    Add UnqiueEntity constraint on cart name

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -8,10 +8,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=CartRepository::class)
+ * @UniqueEntity("name", message="Ce panier existe déjà")
  */
 class Cart
 {
