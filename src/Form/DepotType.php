adbcd215f9838d22d4c8d2146d03f4f10c951337
Steve-Amat <steve.sfa.amat@free.fr>
2022-09-05 16:25:08 +0200

    Depot edition

--- src/Form/DepotType.php
+++ src/Form/DepotType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\Depot;
 use App\Utils\MySlugger;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +16,16 @@ class DepotType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('phone')
-            ->add('address');
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('address', TextareaType::class, [
+                'label' => 'Adresse',
+                'attr' => ['rows' => '3']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
