ab86ed96f83e552e4c6ca482964b342f1738675f
Steve Amat <steve.sfa.amat@free.fr>
2022-11-03 14:14:02 +0100

    Modify Foreign key constraint

--- migrations/Version20221103130132.php
+++ migrations/Version20221103130132.php
@@ -20,7 +20,9 @@ final class Version20221103130132 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` CHANGE user_id user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE `order` CHANGE user_id user_id INT DEFAULT NULL, DROP FOREIGN KEY `FK_F5299398A76ED395`, ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE RESTRICT');
+
+
     }
 
     public function down(Schema $schema): void
